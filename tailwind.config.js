69304f17fbe7e578a44bc400085f96c65191082b
Miljan Gajic <miljan.gajic@bsdex.de>
2023-06-16 12:52:02 +0200

    configuring tailwind and adding some cutom css properties

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "light-bg": "var(--light-bg)",
+        "dark-bg": "var(--dark-bg)",
+        "badge-clr": "var(--badge-clr)",
+        "light-font-clr": "var(--light-font-clr)",
+        "light-hover-clr": "var(--light-hover-clr)",
+        "light-pressed-clr": "var(--light-pressed-clr)",
+        "dark-font-clr": "var(--dark-font-clr)",
+        "dark-hover-clr": "var(--dark-hover-clr)",
+        "dark-pressed-clr": "var(--dark-pressed-clr)",
+        "section-light": "var(--section-light)",
+        "section-dark": "var(--section-dark)",
+      },
+    },
   },
   plugins: [],
-}
-
+};
