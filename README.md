2d2713de19c9c148fd1a50c9eb72f71b8b5c1340
Miljan Gajic <miljan.gajic@bsdex.de>
2023-06-16 13:11:21 +0200

    added some additional configurations

---
+++ README.md
@@ -0,0 +1,7 @@
+#This is a Vite bootstrapped React Application for presenting a Sidebar component
+
+## Packages added
+
+### Node types
+
+This is a dev package to stop showing the errors in Vite config since we need additional config for import aliases to make DX a bit better
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import Content from "@/components/Content/Content";
+import Sidebar from "@/components/Sidebar/Sidebar";
 import "./App.css";
 
 function App() {
-  return <div className="container w-full md:max-w-none lg:container"></div>;
+  return (
+    <div className="container w-full md:max-w-none lg:container">
+      <div className="flex">
+        <Sidebar />
+        <Content />
+      </div>
+    </div>
+  );
 }
 
 export default App;
