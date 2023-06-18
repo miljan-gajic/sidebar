4588aa08da3f597e01d10d70bcd1192964bd47e1
Miljan Gajic <miljan.gajic@bsdex.de>
2023-06-18 22:58:30 +0200

    removed an error from console when loading the icon

--- src/hooks/icons.ts
+++ src/hooks/icons.ts
@@ -1,9 +1,13 @@
 import React, { useEffect, useRef, useState } from "react";
+import { useUI } from "./uiContext";
 
 export function useDynamicSvgImport(iconName: string) {
   const importedIconRef = useRef<React.FC<React.SVGProps<SVGElement>>>();
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<unknown>();
+  const {
+    state: { systemColorTheme },
+  } = useUI();
 
   useEffect(() => {
     setLoading(true);
@@ -11,10 +15,13 @@ export function useDynamicSvgImport(iconName: string) {
     const importSvgIcon = async (): Promise<void> => {
       // all icons must be placed in the same directory
       // if we want that part to be configurable then instead of iconName we will send iconPath as prop
+      // Added the check for systemColorTheme because I'm loading the correct icon based on the mode
       try {
-        importedIconRef.current = (
-          await import(`../assets/icons/${iconName}.svg`)
-        ).ReactComponent; // svgr provides ReactComponent for given svg path
+        if (systemColorTheme) {
+          importedIconRef.current = (
+            await import(`../assets/icons/${iconName}.svg`)
+          ).ReactComponent; // svgr provides ReactComponent for given svg path
+        }
       } catch (err) {
         setError(err);
         console.error(err);
